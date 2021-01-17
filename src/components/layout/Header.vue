ee1cfb47e560530f949b747da18aea70a100c5cc
Ray Bullard <hello@raybullard.com>
2021-01-17 11:46:00 -0800

    Continue todos

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <Header class="app-header">
+        <h1>TodoList</h1>
+    </Header>
+</template>
+
+<script>
+export default {
+    name: "header"
+}
+</script>
+
+<style scoped>
+    .app-header {
+        background: #333;
+        color:#fff;
+        text-align: center;
+        padding:10px;
+    }
+
+    .app-header a {
+        color:#fff;
+        padding-right:5px;
+    }
+</style>
